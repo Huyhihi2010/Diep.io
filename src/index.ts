63c818c2296ca4ba923ce793aee730677aa681b7
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-23 22:47:06 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -124,9 +124,10 @@ server.listen(PORT, () => {
     const teams = new GameServer(wss, "teams", "2Team");
     
     const dom4 = new GameServer(wss, "dom4", "4Domination");
+    const atk = new GameServer(wss, "atk", "Defferent);
 
     // games.push(test, ffa, sbx, maze, dom, duma, teams4, teams, domtest, jungle, factest, spike, ball);
-    games.push(ffa, dom, duma, dom4, teams4, teams, sbx, domtest, jungle, ball);
+    games.push(ffa, dom, duma, dom4, teams4, teams, sbx, domtest, jungle, ball, atk);
 
     util.saveToLog("Servers up", "All servers booted up.", 0x37F554);
     util.log("Dumping endpoint -> gamemode routing table");

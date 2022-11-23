f0c31eee990dbf0bb8478df08a241cc0f47bf3a7
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-23 22:46:04 +0700

    Update Game.ts

--- src/Game.ts
+++ src/Game.ts
@@ -42,6 +42,7 @@ import DominationTestingArena from "./Gamemodes/Misc/DomTest";
 import JungleArena from "./Gamemodes/Misc/Jungle";
 import FactoryTestArena from "./Gamemodes/Misc/FactoryTest";
 import BallArena from "./Gamemodes/Misc/Ball";
+import AttackArena from "./Gamemodes/Attack";
 
 /**
  * WriterStream that broadcasts to all of the game's WebSockets.
@@ -63,13 +64,14 @@ class WSSWriterStream extends Writer {
     }
 }
 
-type DiepGamemodeID = "ffa" | "sandbox" | "teams" | "4teams" | "mot" | "dom" | "dum" | "dom4" | "maze" | "tag" | "survival" | "testing" | "spike" | "domtest" | "jungle" | "factest" | "ball";
+type DiepGamemodeID = "ffa" | "sandbox" | "teams" | "4teams" | "mot" | "dom" | "dum" | "atk" | "dom4" | "maze" | "tag" | "survival" | "testing" | "spike" | "domtest" | "jungle" | "factest" | "ball";
 
 const GamemodeToArenaClass: Record<DiepGamemodeID, (typeof ArenaEntity) | null> & { "*": typeof ArenaEntity }= {
     "ffa": FFAArena,
     "teams": Teams2Arena,
     "4teams": Teams4Arena,
     "sandbox": SandboxArena,
+    "atk": ,
     "*": SandboxArena,
     "dom": DominationArena,
     "dom4": Domination4Arena,

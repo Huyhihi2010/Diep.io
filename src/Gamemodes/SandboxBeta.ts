cd1726030d3d1ddb30f4988a062b4f489de6503c
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-24 20:28:25 +0700

    Update SandboxBeta.ts

--- src/Gamemodes/SandboxBeta.ts
+++ src/Gamemodes/SandboxBeta.ts
@@ -57,29 +57,17 @@ export default class SandboxArena extends ArenaEntity {
 
 		this.updateBounds(arenaSize * 2, arenaSize * 2);
 
-            new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, false), 0);
-	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0, 600, 600, false), 0);
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
+            new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, false), 1);
 	    
-// 	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0 - 600, 600, 600, false), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0 + 600, 600, 600, false), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0 + 600, 600, 600, false), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0 - 600, 600, 600, false), 1);
+	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0 - 600, 600, 600, false), 1);
+	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0 + 600, 600, 600, false), 1);
+	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0 + 600, 600, 600, false), 1);
+	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0 - 600, 600, 600, false), 1);
 	    
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0 - 600, 600, 600, false), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0, 600, 600, false), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0 + 600, 600, 600, false), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0, 600, 600, false), 1);
-	    
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
-	    
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
-// 	    new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true), 1);
+	    new Dominator(this, new TeamBase(game, this, 0, 0 - 600, 600, 600, false), 1);
+	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0, 600, 600, false), 1);
+	    new Dominator(this, new TeamBase(game, this, 0, 0 + 600, 600, 600, false), 1);
+	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0, 600, 600, false), 1);
 	   
 
 		// const w1 = new MazeWall(this.game, 0, 0, 500, 500);

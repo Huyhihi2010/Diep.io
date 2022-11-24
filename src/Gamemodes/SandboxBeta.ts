88cf99ba455520cde380e5bf5f8f7311df0c8067
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-24 13:19:21 +0700

    Update SandboxBeta.ts

--- src/Gamemodes/SandboxBeta.ts
+++ src/Gamemodes/SandboxBeta.ts
@@ -56,11 +56,12 @@ export default class SandboxArena extends ArenaEntity {
 
 		this.updateBounds(arenaSize * 2, arenaSize * 2);
 
-            new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, true));
-	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0 - 600, 600, 600, true));
-	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0 + 600, 600, 600, true));
-	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0 + 600, 600, 600, true));
-	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0 - 600, 600, 600, true));
+            new Dominator(this, new TeamBase(game, this, 0, 0, 600, 600, false));
+	    
+	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0 - 600, 600, 600, false));
+	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0 + 600, 600, 600, false));
+	    new Dominator(this, new TeamBase(game, this, 0 - 600, 0 + 600, 600, 600, false));
+	    new Dominator(this, new TeamBase(game, this, 0 + 600, 0 - 600, 600, 600, false));
 
 		// const w1 = new MazeWall(this.game, 0, 0, 500, 500);
     }

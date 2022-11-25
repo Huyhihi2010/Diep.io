5bf35388bf0a3643dd1a3a3760252fd08c1de608
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-25 21:06:11 +0700

    Update FallenArenaCloser.ts

--- src/Entity/Misc/FallenArenaCloser.ts
+++ src/Entity/Misc/FallenArenaCloser.ts
@@ -26,7 +26,7 @@ import { AI, AIState, Inputs } from "../AI";
 /**
  * Represents the Arena Closers that end the game.
  */
-export default class ArenaCloser extends TankBody {
+export default class FallenArenaCloser extends TankBody {
     /** Size of a level 0 Arena Closer. */
     public static BASE_SIZE = 225;
     
@@ -41,9 +41,9 @@ export default class ArenaCloser extends TankBody {
         camera.setLevel = function(level) {
             setLevel.call(this, level);
 
-            this.sizeFactor *= (ArenaCloser.BASE_SIZE / 50);
+            this.sizeFactor *= (FallenArenaCloser.BASE_SIZE / 50);
         }
-        camera.sizeFactor = (ArenaCloser.BASE_SIZE / 50);
+        camera.sizeFactor = (FallenArenaCloser.BASE_SIZE / 50);
 
         super(game, camera, inputs);
 
@@ -55,7 +55,7 @@ export default class ArenaCloser extends TankBody {
 
         this.health.values.healthbar |= HealthbarFlags.hidden;
 
-        this.setTank(Tank.ArenaCloser);
+        this.setTank(Tank.FallenArenaCloser);
 
         const def = (this.definition = Object.assign({}, this.definition));
         def.maxHealth = 100000;

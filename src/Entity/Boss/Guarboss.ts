5e36ab63d9fb300d49661005df7a18ae45c8515e
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-25 19:27:51 +0700

    Update Guarboss.ts

--- src/Entity/Boss/Guarboss.ts
+++ src/Entity/Boss/Guarboss.ts
@@ -76,7 +76,7 @@ export default class Guardian extends AbstractBoss {
             size: 120 / (1.01 ** (75 - 1)),
             width: 91.4 / (1.01 ** (75 - 1)),
             delay: 0,
-            reload: 5,
+            reload: 0.5,
             recoil: 0,
             isTrapezoid: true,
             trapezoidDirection: 0,

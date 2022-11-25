18b052a3273e7eac3a05f0c7dfdd28f10b6d6992
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-25 16:57:08 +0700

    Update Guarboss.ts

--- src/Entity/Boss/Guarboss.ts
+++ src/Entity/Boss/Guarboss.ts
@@ -62,14 +62,39 @@ export default class Guardian extends AbstractBoss {
             }
         }));
         this.barrels.push(new Barrel(this, {
-            angle: Math.PI * 0.5,
+            angle: Math.PI * 0.75,
             offset: 0,
             // Scale cuz direct
             size: 100 / (1.01 ** (75 - 1)),
             width: 71.4 / (1.01 ** (75 - 1)),
             delay: 0,
             reload: 0.25,
-            recoil: 2,
+            recoil: 0,
+            isTrapezoid: true,
+            trapezoidDirection: 0,
+            addon: null,
+            droneCount: 50,
+            canControlDrones: true,
+            bullet: {
+                type: "drone",
+                sizeRatio: 0.6,
+                health: 2.5,
+                damage: 1,
+                speed: 1.7,
+                scatterRate: 1,
+                lifeLength: 1,
+                absorbtionFactor: 1
+            }
+        }));
+        this.barrels.push(new Barrel(this, {
+            angle: -Math.PI * 0.75,
+            offset: 0,
+            // Scale cuz direct
+            size: 100 / (1.01 ** (75 - 1)),
+            width: 71.4 / (1.01 ** (75 - 1)),
+            delay: 0,
+            reload: 0.25,
+            recoil: 0,
             isTrapezoid: true,
             trapezoidDirection: 0,
             addon: null,

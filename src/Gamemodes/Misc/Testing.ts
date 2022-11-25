91513006a9896f26d838ac83c1c6407b184dbe05
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-25 17:09:35 +0700

    Update Testing.ts

--- src/Gamemodes/Misc/Testing.ts
+++ src/Gamemodes/Misc/Testing.ts
@@ -65,16 +65,17 @@ export default class TestingArena extends ArenaEntity {
 //                 new Summoner(game);
 //             }
 //         }, 60000)
+        
+        new Guarboss(game);
 
         setInterval(() => {
 //             new Guardian(game);
-            new Guarboss(game);
-            new AbstractBoss(game);
+           // new AbstractBoss(game);
 //             new Summoner(game);
 //             new FallenMegaTrapper(game);
 //             new FallenAC(game);
 //             new FallenBooster(game);
-//             new FallenOverlord(game);
+            new FallenOverlord(game);
 //             new FallenSpike(game);
         }, 8000)
 

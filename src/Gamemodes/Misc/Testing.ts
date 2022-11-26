78d1d740b54b48e68e2da292352a15064f119da0
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-26 19:03:38 +0700

    Update Testing.ts

--- src/Gamemodes/Misc/Testing.ts
+++ src/Gamemodes/Misc/Testing.ts
@@ -79,8 +79,8 @@ export default class TestingArena extends ArenaEntity {
 //             new Guarboss(game);
             for(var i = 0; i < 12; i++) {
 //                 new BoosterArenaCloser(game);
-//                 new PentaArenaCloser(game);
-                new FactoryArenaCloser(game);
+                new PentaArenaCloser(game);
+//                 new FactoryArenaCloser(game);
             }
 //             new TwinArenaCloser(game);
 //             new ArenaCloser(game);

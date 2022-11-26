f122dec003e18b87b073176e12562c21f4fc12db
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-26 12:57:41 +0700

    Update Enums.ts

--- src/Const/Enums.ts
+++ src/Const/Enums.ts
@@ -124,7 +124,8 @@ export enum Tank {
     Factory       = 52,
     Skimmer       = 54,
     Rocketeer     = 55,
-    FallenArenaCloser = 56
+    FallenArenaCloser = 56,
+    TwinArenaCloser = 57
 }
 
 /**

89a90618f21ddacff4390aad202b27428230e118
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-26 15:57:27 +0700

    Update Testing.ts

--- src/Gamemodes/Misc/Testing.ts
+++ src/Gamemodes/Misc/Testing.ts
@@ -72,11 +72,13 @@ export default class TestingArena extends ArenaEntity {
         setTimeout(() => {
 //             new Guarboss(game);
             new TwinArenaCloser(game);
+            new ArenaCloser(game);
         }, 1000)
 //         setInterval(() => {
 //             new FallenGura(game);
 //         }, 64000)
 //         setTimeout(() => {
+            new FallenArenaCloser(game);
             new FallenArenaCloser(game);
 //         }, 64000);
 
